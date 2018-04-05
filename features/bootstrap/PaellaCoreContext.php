c6ff0a3b8728ef4fbaa10fefe1bef9f400d689e2
alahtarin <alex.lahtarin@gmail.com>
2018-04-05 17:45:28 +0200

    More behat playaround

--- features/bootstrap/PaellaCoreContext.php
+++ features/bootstrap/PaellaCoreContext.php
@@ -1,7 +1,14 @@
 <?php
 
 use Behat\MinkExtension\Context\MinkContext;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class PaellaCoreContext extends MinkContext
 {
+    private $kernel;
+
+    public function __construct(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
 }
