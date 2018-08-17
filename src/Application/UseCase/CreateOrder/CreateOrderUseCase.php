ccc2fa0545153df1ace0dc2cbe6702fa6eca1be7
dabredo <da.breva@gmail.com>
2018-08-17 16:43:48 +0200

    OZ-2295 Send person name when identify debtor (#111)

--- src/Application/UseCase/CreateOrder/CreateOrderUseCase.php
+++ src/Application/UseCase/CreateOrder/CreateOrderUseCase.php
@@ -158,6 +158,8 @@ class CreateOrderUseCase implements LoggingInterface
             'registration_number' => $orderContainer->getDebtorExternalData()->getRegistrationNumber(),
             'registration_court' => $orderContainer->getDebtorExternalData()->getRegistrationCourt(),
             'legal_form' => $orderContainer->getDebtorExternalData()->getLegalForm(),
+            'firstname' => $orderContainer->getDebtorPerson()->getFirstName(),
+            'lastname' => $orderContainer->getDebtorPerson()->getLastName(),
         ]);
     }
 
