629ebc21a71f7970f367756e01d7bebe2bfde5e5
alahtarin <alex.lahtarin@gmail.com>
2019-04-30 15:29:07 +0200

    More forgiveness logs

--- src/DomainModel/OrderPayment/OrderPaymentForgivenessService.php
+++ src/DomainModel/OrderPayment/OrderPaymentForgivenessService.php
@@ -6,9 +6,13 @@ use App\DomainModel\Borscht\BorschtInterface;
 use App\DomainModel\Borscht\OrderAmountChangeDTO;
 use App\DomainModel\MerchantSettings\MerchantSettingsRepositoryInterface;
 use App\DomainModel\Order\OrderEntity;
+use Billie\MonitoringBundle\Service\Logging\LoggingInterface;
+use Billie\MonitoringBundle\Service\Logging\LoggingTrait;
 
-class OrderPaymentForgivenessService
+class OrderPaymentForgivenessService implements LoggingInterface
 {
+    use LoggingTrait;
+
     private $paymentsService;
 
     private $merchantSettingsRepository;
@@ -26,6 +30,13 @@ class OrderPaymentForgivenessService
         $outstandingAmount = $amountChange->getOutstandingAmount();
         $merchantSettings = $this->merchantSettingsRepository->getOneByMerchantOrFail($order->getMerchantId());
         $debtorForgivenessThreshold = $merchantSettings->getDebtorForgivenessThreshold();
+
+        $this->logInfo('Checking for forgiveness', [
+            'forgiveness_threshold' => $debtorForgivenessThreshold,
+            'outstanding_amount' => $outstandingAmount,
+            'paid_amount' => $amountChange->getPaidAmount(),
+            'amount_higher_than_threshold' => $outstandingAmount > $debtorForgivenessThreshold,
+        ]);
 
         if (
             ($debtorForgivenessThreshold <= 0) ||
