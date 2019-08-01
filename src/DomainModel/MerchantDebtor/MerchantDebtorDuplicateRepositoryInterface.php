c56f87bc98504ff355f3b125437ed5dbb28bb35c
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2019-08-01 11:43:23 +0200

    remove uncool paella:identify-debtor-duplicates related code (#381)

--- src/DomainModel/MerchantDebtor/MerchantDebtorDuplicateRepositoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\DomainModel\MerchantDebtor;
-
-interface MerchantDebtorDuplicateRepositoryInterface
-{
-    public function upsert(MerchantDebtorDuplicateEntity $merchantDebtorDuplicate): MerchantDebtorDuplicateEntity;
-}
