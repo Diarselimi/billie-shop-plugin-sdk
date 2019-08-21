c22aa38b4b7508c32198194070e344869e9d0b0f
Diar <diarselimi92@gmail.com>
2019-08-21 13:40:52 +0200

    APIS-1055 Changed the hasher logic (#384)
    * Changed the hasher logic
    
    * Updated the bdd test
    
    * Changed the hasher logic to remove othe white spaces and handle nested arrays

--- src/Helper/Hasher/ArrayHasher.php
+++ src/Helper/Hasher/ArrayHasher.php
@@ -3,18 +3,25 @@
 namespace App\Helper\Hasher;
 
 use App\DomainModel\ArrayableInterface;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 class ArrayHasher implements ArrayHasherInterface
 {
+    const ESCAPE_CHARACTERS = "!#@$%^&*() {}[]:;.,'?<>/\\\t\n\r\0\x0B";
+
     public function generateHash(ArrayableInterface $object, array $ignoreFields = []): string
     {
-        $preparedArray = array_diff_key($object->toArray(), array_flip($ignoreFields));
+        $data = Arr::dot($object->toArray());
+        $preparedArray = array_diff_key($data, array_flip($ignoreFields));
 
-        $stringifiedData = preg_replace(
-            '/[^A-Za-z0-9äöüßé]/',
-            '',
-            mb_strtolower(implode("", $preparedArray))
+        $stringifiedData = trim(
+            Str::lower(
+                implode(' ', $preparedArray)
+            ),
+            self::ESCAPE_CHARACTERS
         );
+        $stringifiedData = preg_replace('/\s+/', ' ', $stringifiedData);
 
         return md5($stringifiedData);
     }
