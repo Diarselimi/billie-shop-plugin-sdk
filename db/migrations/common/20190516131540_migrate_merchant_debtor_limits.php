12157f5e8d20735b2279ddda262ad4216675735a
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2020-06-15 09:35:01 +0200

    CORE-206 tech-debt: remove borscht DB dependency (#612)
    * tech-debt: remove borscht DB dependency
    
    * Revert "tech-debt: remove borscht DB dependency"
    
    This reverts commit 9d66088b0fed5d0a49b7cd0a90c70684377f9b62.
    
    * remove migration content
    
    * remove one-off update migration involving borscht DB

--- db/migrations/common/20190516131540_migrate_merchant_debtor_limits.php
+++ db/migrations/common/20190516131540_migrate_merchant_debtor_limits.php
@@ -12,36 +12,7 @@ class MigrateMerchantDebtorLimits extends AbstractMigration
      */
     public function change()
     {
-        $this->query("
-            UPDATE merchants_debtors
-            SET financing_limit = financing_limit + (SELECT COALESCE(SUM(orders.amount_gross), 0)
-            FROM orders
-            WHERE orders.state = 'new'
-            AND orders.merchant_debtor_id = merchants_debtors.id
-            AND EXISTS (
-                SELECT * FROM order_risk_checks
-                WHERE order_risk_checks.order_id = orders.id
-                AND order_risk_checks.risk_check_definition_id = 6
-                AND order_risk_checks.is_passed = 1
-            ));
-            
-            INSERT INTO merchant_debtor_financial_details 
-            (merchant_debtor_id, financing_limit, financing_power, created_at)
-            SELECT id, financing_limit, financing_limit, NOW() FROM merchants_debtors;
-            
-            UPDATE merchant_debtor_financial_details
-            SET merchant_debtor_financial_details.financing_limit = merchant_debtor_financial_details.financing_limit + COALESCE(
-                (SELECT sum(amount_gross) FROM orders WHERE orders.state = 'created' AND orders.merchant_debtor_id = merchant_debtor_financial_details.merchant_debtor_id)
-                , 0
-            );
-            
-            UPDATE merchant_debtor_financial_details
-            INNER JOIN merchants_debtors on merchant_debtor_financial_details.merchant_debtor_id = merchants_debtors.id
-            INNER JOIN borscht.debtors on borscht.debtors.uuid = merchants_debtors.payment_debtor_id
-            SET merchant_debtor_financial_details.financing_limit = merchant_debtor_financial_details.financing_limit + COALESCE(
-                (SELECT SUM(outstanding_amount) FROM borscht.tickets WHERE borscht.tickets.debtor_id = borscht.debtors.id)
-                , 0
-            );
-        ");
+        // This was a one-off migration using joins with Borscht DB.
+        // Kept for keeping migrations log consistency.
     }
 }
