e7abcf55797e2b93c37a4579b8cc130973422104
Diar <diar@billie.io>
2020-07-02 13:47:22 +0200

    CORE-189 Switch to new verion of amqp, two messages  (#631)
    * Switch to new verions of amqp, two messages
    
    * Added the missing handler
    
    * Pr regards
    
    * Remove not needed file
    
    * Update billie.yaml
    
    * Update billie.yaml
    
    Co-authored-by: schneekatze <schneekatze@users.noreply.github.com>

---
+++ src/Amqp/Handler/OrderDunningStepChangedHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Amqp\Handler;
+
+use App\Application\UseCase\UpdateMerchantWithOrderDunningStep\UpdateMerchantWithOrderDunningStepRequest;
+use App\Application\UseCase\UpdateMerchantWithOrderDunningStep\UpdateMerchantWithOrderDunningStepUseCase;
+use Ozean12\Transfer\Message\Order\OrderDunningStepChanged;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+class OrderDunningStepChangedHandler implements MessageHandlerInterface
+{
+    private $useCase;
+
+    public function __construct(UpdateMerchantWithOrderDunningStepUseCase $useCase)
+    {
+        $this->useCase = $useCase;
+    }
+
+    public function __invoke(OrderDunningStepChanged $message)
+    {
+        $request = new UpdateMerchantWithOrderDunningStepRequest(
+            $message->getUuid(),
+            $message->getDunningStep()
+        );
+
+        $this->useCase->execute($request);
+    }
+}
