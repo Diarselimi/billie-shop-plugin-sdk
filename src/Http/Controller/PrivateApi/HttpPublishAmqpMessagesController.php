a8e0387cf2ab1191961dfa957017ac43453c45c9
schneekatze <schneekatze@users.noreply.github.com>
2020-10-23 11:31:05 +0200

    CORE-449 Publish AMQP Messages via HTTP (#729)
    * CORE-449 Publish AMQP Messages via HTTP
    https://ozean12.atlassian.net/browse/CORE-449
    
    * cr fixes
    
    * rm routes
    
    * OA
    
    * Increase borscht timeout
    
    * align makefile with app skeleton and fix docker-comp. (#727)
    
    * align makefile with app skeleton and fix docker-comp.
    
    * fix tests
    
    * update packages
    
    * CORE-358 Document validation error in checkout widget docs (#726)
    
    * CORE-358 Document validation error in checkout widget docs
    
    * generate docs
    
    Co-authored-by: Javi Aguilar <122741+itsjavi@users.noreply.github.com>
    
    * Improve the order container loading (#728)
    
    * Improve the order container loading
    
    * CS
    
    * Spec fixes
    
    * No ws
    
    * Fix code style
    
    * Add the repo
    
    * Fix code style
    
    * Stop behat for test
    
    * Fixes with address
    
    * Enable behat
    
    * Enable behat
    
    * Beter step in behat
    
    * Other behat tests
    
    * Fixes
    
    * Two last fixes
    
    * Bad Alex
    
    * Remove unneeded changes
    
    * CS
    
    * CR
    
    * CR2
    
    Co-authored-by: Diar <diar@billie.io>
    
    * t
    
    * openapi docs
    
    * Add request body
    
    * Change route
    
    * revert the descriptions
    
    Co-authored-by: alahtarin <alex.lahtarin@gmail.com>
    Co-authored-by: Javi Aguilar <122741+itsjavi@users.noreply.github.com>
    Co-authored-by: Miroslav Saračević <msaracevic@users.noreply.github.com>
    Co-authored-by: Diar <diar@billie.io>

---
+++ src/Http/Controller/PrivateApi/HttpPublishAmqpMessagesController.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controller\PrivateApi;
+
+use Billie\MonitoringBundle\Service\RidProvider;
+use Enqueue\MessengerAdapter\EnvelopeItem\TransportConfiguration;
+use Google\Protobuf\Internal\Message;
+use Ozean12\AmqpPackBundle\Mapping\AmqpMapperInterface;
+use Ozean12\AmqpPackBundle\Transport\RequestIdStamp;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\MessageBusInterface;
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Post(
+ *     path="/publish-to-amqp",
+ *     operationId="publish_to_amqp",
+ *     summary="Publish a message to the amqp broker",
+ *
+ *     x={"groups":{"private"}},
+ *
+ *     @OA\RequestBody(
+ *          required=true,
+ *          @OA\MediaType(mediaType="application/json",
+ *          @OA\Schema(type="object", properties={
+ *              @OA\Property(property="class_name", type="string", description="What message to publish"),
+ *              @OA\Property(property="json", type="array", description="Message's body")
+ *          }))
+ *     ),
+ *
+ *     @OA\Response(response=202),
+ *     @OA\Response(response=500, ref="#/components/responses/ServerError")
+ * )
+ */
+class HttpPublishAmqpMessagesController
+{
+    /**
+     * @var MessageBusInterface
+     */
+    private $messageBus;
+
+    /**
+     * @var AmqpMapperInterface
+     */
+    private $amqpMapper;
+
+    /**
+     * @var RidProvider
+     */
+    private $requestIdProvider;
+
+    public function __construct(
+        MessageBusInterface $messageBus,
+        AmqpMapperInterface $amqpMapper,
+        RidProvider $requestIdProvider
+    ) {
+        $this->messageBus = $messageBus;
+        $this->amqpMapper = $amqpMapper;
+        $this->requestIdProvider = $requestIdProvider;
+    }
+
+    public function execute(Request $request)
+    {
+        $className = $request->get('class_name');
+        $json = $request->get('json');
+
+        if (empty($className) or empty($json)) {
+            throw new BadRequestHttpException('Either "class_name" is empty or "json" in the POST request');
+        }
+
+        $className = $this->amqpMapper->mapToClassName($className);
+
+        /**
+         * @var Message $message
+         */
+        $message = new $className;
+        $message->mergeFromJsonString(\GuzzleHttp\json_encode($json));
+        $this->produceMessage($message);
+
+        return new Response('', Response::HTTP_ACCEPTED);
+    }
+
+    public function produceMessage($message)
+    {
+        $this->messageBus->dispatch(
+            (new Envelope($message))
+                ->with(new RequestIdStamp($this->requestIdProvider->getRid()))
+                ->with(
+                    new TransportConfiguration(
+                        [
+                            'topic' => 'front',
+                            'metadata' => [
+                                'routingKey' => $this->amqpMapper->mapToKey(get_class($message)),
+                            ],
+                        ]
+                    )
+                )
+        );
+    }
+}
