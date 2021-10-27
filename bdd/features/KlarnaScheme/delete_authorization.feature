e79f7d9d14b2e7ff66a62ed5f5980831f2a4c013
Diar <diar@billie.io>
2021-10-27 16:00:08 +0200

    CORE-941 Delete authorization endpoint (#967)
    * Delete authorization endpoint
    
    * remove body request
    
    * switch to interface
    
    Co-authored-by: Felipe Martins <6557756+fefas@users.noreply.github.com>

---
+++ bdd/features/KlarnaScheme/delete_authorization.feature
@@ -0,0 +1,33 @@
+Feature:
+  Purpose - Delete an unconfirmed Authorization.
+  Preconditions - The payment_method_reference refers to an unconfirmed Authorization.
+  Scheme operations - The Authorization is deleted. The delete notification should always be accepted by the Payment Method.
+
+  Background:
+    Given I have orders with the following data
+      | uuid                 | external_id | state      | gross | net | tax | duration | payment_uuid                         |
+      | confirmed-order-uuid | C3PO        | authorized | 1000  | 900 | 100 | 30       | 6d6b4222-be8c-11e9-9cb5-2a2ae2dbcce4 |
+    And GraphQL will respond to getMerchantDebtorDetails query
+
+  Scenario: Authorization does not exist.
+    When I request "POST /authorizations/00112233-4455-6677-8899-aabbccddeeff/delete" with body:
+      """
+      {
+      }
+      """
+    Then the response is 200 with body:
+      """
+      {
+        "error_messages": [ "Authorization not found" ]
+      }
+      """
+
+  Scenario: Successfully delete the authorization.
+    When I request "POST /authorizations/confirmed-order-uuid/delete" with body:
+      """
+      {
+      }
+      """
+    Then the response is 200
+    And the order "C3PO" is in state "declined"
+
---
+++ src/UserInterface/Http/KlarnaScheme/DeleteAuthorization/DeleteAuthorizationController.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UserInterface\Http\KlarnaScheme\DeleteAuthorization;
+
+use App\Application\CommandBus;
+use App\Application\Exception\OrderNotFoundException;
+use App\Application\Exception\WorkflowException;
+use App\Application\UseCase\DeclineOrder\DeclineOrderRequest;
+use App\UserInterface\Http\KlarnaScheme\KlarnaResponse;
+use Billie\MonitoringBundle\Service\Logging\LoggingInterface;
+use Billie\MonitoringBundle\Service\Logging\LoggingTrait;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class DeleteAuthorizationController implements LoggingInterface
+{
+    use LoggingTrait;
+
+    private CommandBus $commandBus;
+
+    public function __construct(CommandBus $commandBus)
+    {
+        $this->commandBus = $commandBus;
+    }
+
+    public function execute(Request $request, string $orderId): JsonResponse
+    {
+        $command = new DeclineOrderRequest($orderId);
+
+        try {
+            $this->commandBus->process($command);
+        } catch (OrderNotFoundException $orderNotFoundException) {
+            return KlarnaResponse::withErrorMessage('Authorization not found');
+        } catch (WorkflowException $ex) {
+            $this->logSuppressedException($ex, 'Klarna tried to decline a confirmed authorization.');
+
+            return KlarnaResponse::withErrorMessage('Delete Authorization is not possible.');
+        }
+
+        return new JsonResponse('', JsonResponse::HTTP_OK);
+    }
+}
